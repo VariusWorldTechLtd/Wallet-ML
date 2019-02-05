12380697a93c72ae47792058cf5b22dee637ebaa
Sam Peek <sam.j.peek@gmail.com>
2019-02-05 11:03:46 +0000

    Add detailed Step Functions

--- AWS_Framework/Updating/README.md
+++ AWS_Framework/Updating/README.md
@@ -5,3 +5,9 @@
 ## Overview
 
 This folder contains the javascript code of functions that can be used within the step functions. For example, the CreateTrainingJob file contains all code to train a new model and save the output.
+
+## Template
+
+The follwoing URL links to a tutorial on creating a CloudFormation Stack which should contain all services needed for the updating and redeployment. Note that this not only works using Step Functions and CloudWatch but also services within the SageMaker architecture.
+
+https://aws.amazon.com/blogs/machine-learning/automated-and-continuous-deployment-of-amazon-sagemaker-models-with-aws-step-functions/
---
+++ Development/Lambda/Sagebuild/StepLambdaStartHPO.js
@@ -0,0 +1,96 @@
+var aws=require('aws-sdk')
+aws.config.region=process.env.AWS_REGION 
+var crypto = require('crypto');
+var sagemaker=new aws.SageMaker()
+
+exports.handler=(event,context,cb)=>{
+    console.log("EVENT:",JSON.stringify(event,null,2))
+    var shasum = crypto.createHash('sha1');
+    try{
+        event.args.training.Tags=event.args.training.Tags || []
+        event.args.training.Tags.push({
+            Key:"sagebuild:BuildStack",
+            Value:event.params.stackname
+        })
+        hyperParams={
+            static:{},
+            catagorical:[],
+            continuous:[],
+            integer:[]
+        }
+        old_args=event.args.training
+        Object.keys(old_args.HyperParameters).forEach(function(key){
+            val=old_args.HyperParameters[key]
+            try{
+                parsed=JSON.parse(val)
+            }catch(e){
+                parsed=val     
+            }
+
+            if(typeof(parsed)==="object"){
+                parsed.Name=key
+                if(parsed.Values){
+                    hyperParams.catagorical.push(parsed)
+                }else if(typeof(parsed.MaxValue)!=="undefined"){
+                    if(parsed.integer){
+                        delete parsed.integer
+                        hyperParams.integer.push(parsed)
+                    }else{
+                        delete parsed.integer
+                        hyperParams.continuous.push(parsed)
+                    }
+                }else{
+                    hyperParams.static[key]=val
+                }
+            }else{
+                hyperParams.static[key]=val
+            }
+        })
+        shasum.update(old_args.TrainingJobName);
+
+        args={
+            HyperParameterTuningJobConfig:{
+                HyperParameterTuningJobObjective:{
+                    MetricName:event.params.tuningobjective.Name,
+                    Type:event.params.tuningobjective.Type
+                },
+                ParameterRanges:{
+                    CategoricalParameterRanges:hyperParams.catagorical,
+                    ContinuousParameterRanges:hyperParams.continuous,
+                    IntegerParameterRanges:hyperParams.integer
+                },
+                ResourceLimits:{
+                    MaxNumberOfTrainingJobs:event.params.maxtrainingjobs,
+                    MaxParallelTrainingJobs:event.params.maxparalleltrainingjobs,
+                },
+                Strategy:"Bayesian"
+            },
+            HyperParameterTuningJobName:shasum.digest('hex').slice(0,32),
+            TrainingJobDefinition:{
+                AlgorithmSpecification:{
+                    TrainingImage:old_args.AlgorithmSpecification.TrainingImage,
+                    TrainingInputMode:old_args.AlgorithmSpecification.TrainingInputMode
+                },
+                InputDataConfig:old_args.InputDataConfig,
+                OutputDataConfig:old_args.OutputDataConfig,
+                ResourceConfig:old_args.ResourceConfig,
+                RoleArn:old_args.RoleArn,
+                StoppingCondition:old_args.StoppingCondition,
+                StaticHyperParameters:hyperParams.static,
+            },
+            Tags:old_args.Tags
+        }
+        if(event.params.tuningobjective.Regex){
+            args.TrainingJobDefinition.AlgorithmSpecification.MetricDefinitions=[{
+                Name:event.params.tuningobjective.Name,
+                Regex:event.params.tuningobjective.Regex
+            }]
+        }
+        console.log(JSON.stringify(args,null,2))
+        sagemaker.createHyperParameterTuningJob(args).promise()
+        .then(result=>cb(null,result))
+        .catch(x=>cb(new Error(x)))
+    }catch(e){
+        cb(new Error(e))
+    }
+}
